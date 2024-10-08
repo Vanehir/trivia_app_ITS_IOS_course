188f0dfc73166f68b46cf562f8cae69240375cca
yuri <yuri.meneghin@gmail.com>
2024-10-08 09:51:45 +0200

    Initial Commit

---
+++ trivia app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trivia app
+//
+//  Created by Yuri Meneghin on 08/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trivia app/trivia_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  trivia_appApp.swift
+//  trivia app
+//
+//  Created by Yuri Meneghin on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct trivia_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
